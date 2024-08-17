6b142e8c88c048e9021495de3f1b2bd182491f5f
amir <bloodlean7@gmail.com>
2024-08-17 16:51:42 +0300

    commit2

--- atech/atech/urls.py
+++ atech/atech/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for atech project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'), 
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('search/', views.search_view, name='search_view'),
+    path('advanced_search/', views.advanced_search_view, name='advanced_search'),
+    path('category/<int:category_id>/', views.category_view, name='category_view'),
+    path('cart/', views.view_cart, name='view_cart'),  
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),  
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),  # Исправлено: views.remove_from_cart
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
